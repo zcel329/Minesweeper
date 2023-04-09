aa9c99236484eac4cf60554b23f6ff6b1b34606d
zach <zachcelona@gmail.com>
2023-04-10 03:41:18 +1200

    Adding difficulty selection

--- src/main/java/msc/Main.java
+++ src/main/java/msc/Main.java
@@ -1,13 +1,38 @@
 package msc;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            Board board = new Board();
-            board = board.createBoard(Board.BoardType.BEGINNER);
-            for (int i = 0; i < board.getBoardArray().length; i++) {
-                System.out.println(Arrays.toString(board.getBoardArray()[i]));
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Choose difficulty: \n 1: [BEGINNER] \n 2: [INTERMEDIATE] \n 3: [EXPERT]");
+
+        String selection = myObj.nextLine();  // Read user input
+        int diff = Integer.parseInt(selection);
+        Board.BoardType boardtype;
+        switch (diff) {
+            case 1 -> {
+                boardtype = Board.BoardType.BEGINNER;
+                System.out.println("You have chosen BEGINNER \n");
+            }
+            case 2 -> {
+                boardtype = Board.BoardType.INTERMEDIATE;
+                System.out.println("You have chosen INTERMEDIATE \n");
+            }
+            case 3 -> {
+                boardtype = Board.BoardType.EXPERT;
+                System.out.println("You have chosen EXPERT \n");
+            }
+            default -> {
+                System.out.println("Error: invalid difficulty choice. Defaulted to BEGINNER difficulty.");
+                boardtype = Board.BoardType.BEGINNER;
             }
         }
+        Board board = new Board();
+        board = board.createBoard(boardtype);
+        for (int i = 0; i < board.getBoardArray().length; i++) {
+            System.out.println(Arrays.toString(board.getBoardArray()[i]));
+        }
     }
+}
